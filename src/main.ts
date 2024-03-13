5f4c63aa325a4dd865a255a04c09a9cada197c9d
Athish Thayalan <tathish@hotmail.co.uk>
2024-03-13 10:07:39 +0000

    Imported relevant pokemon and types file to main .ts. And stored container into a variable. Also created a function to generate cards that is called on DOMContentLoad.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import "./styles/style.scss";
+import "./data/pokemon";
+import "./data/types";
+
+const container = document.querySelector<HTMLDivElement>("card-container");
+
+if (!container) {
+  throw new Error("Element not found.");
+}
+
+const generatePokemonCards = (): void => {
+  // card container --> card , card img --> card,content --> card text , card heading
+};
+
+container.addEventListener("DOMContentLoad", generatePokemonCards);
