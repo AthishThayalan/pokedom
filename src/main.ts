aa68345c7288c6fdfb6c82501743ec5eecdc804f
Athish Thayalan <tathish@hotmail.co.uk>
2024-03-13 10:34:33 +0000

    Created card and image elements.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import "./styles/style.scss";
 import "./data/pokemon";
 import "./data/types";
+import pokemonArray from "./data/pokemon";
 
-const container = document.querySelector<HTMLDivElement>("card-container");
+const container = document.querySelector<HTMLDivElement>(".card-container");
+console.log("HELLo");
 
 if (!container) {
   throw new Error("Element not found.");
@@ -10,6 +12,16 @@ if (!container) {
 
 const generatePokemonCards = (): void => {
   // card container --> card , card img --> card,content --> card text , card heading
+  console.log("LOADED");
+  pokemonArray.forEach((pokemon) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const image = document.createElement("img");
+    image.src = pokemon.sprite;
+    image.alt = pokemon.name;
+    card.appendChild(image);
+  });
 };
 
-container.addEventListener("DOMContentLoad", generatePokemonCards);
+document.addEventListener("DOMContentLoaded", generatePokemonCards);
