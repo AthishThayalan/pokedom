753091c09286acf494a46e1e8b0c42347fda1a85
Athish Thayalan <tathish@hotmail.co.uk>
2024-03-13 11:39:59 +0000

    created a seperate function to generate cards , which takes a pokemon object as a parameter and then call this function when needed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./styles/style.scss";
 import "./data/pokemon";
-import "./data/types";
+import { Pokemon } from "./data/types";
 import pokemonArray from "./data/pokemon";
 
 const container = document.querySelector<HTMLDivElement>(".card-container");
@@ -10,85 +10,56 @@ if (!container || !input) {
   throw new Error("Element not found.");
 }
 
-const generatePokemonCards = (): void => {
+const generateCards = (pokemon: Pokemon): void => {
+  const pokemonTypes = pokemon.types;
+  const pokemonTypesText: string =
+    pokemonTypes.length === 1
+      ? `${pokemonTypes[0]}`
+      : `${pokemonTypes[0]} & ${pokemonTypes[1]}`;
+  const pokemonName =
+    pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const image = document.createElement("img");
+  image.src = pokemon.sprite;
+  image.alt = pokemon.name;
+  card.appendChild(image);
+
+  const content = document.createElement("div");
+  content.classList.add("card__content");
+
+  const heading = document.createElement("h2");
+  heading.classList.add("card__heading");
+  heading.innerText = pokemonName;
+  content.appendChild(heading);
+
+  const text = document.createElement("p");
+  text.classList.add("card__text");
+
+  text.innerText = `${pokemonName} (#${pokemon.id}) is a ${pokemonTypesText} type pokemon.`;
+  content.appendChild(text);
+
+  card.appendChild(content);
+
+  container.appendChild(card);
+};
+const displayAllCards = (): void => {
   pokemonArray.forEach((pokemon) => {
-    const pokemonTypes = pokemon.types;
-    const pokemonTypesText: string =
-      pokemonTypes.length === 1
-        ? `${pokemonTypes[0]}`
-        : `${pokemonTypes[0]} & ${pokemonTypes[1]}`;
-    const pokemonName =
-      pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    const image = document.createElement("img");
-    image.src = pokemon.sprite;
-    image.alt = pokemon.name;
-    card.appendChild(image);
-
-    const content = document.createElement("div");
-    content.classList.add("card__content");
-
-    const heading = document.createElement("h2");
-    heading.classList.add("card__heading");
-    heading.innerText = pokemonName;
-    content.appendChild(heading);
-
-    const text = document.createElement("p");
-    text.classList.add("card__text");
-
-    text.innerText = `${pokemonName} (#${pokemon.id}) is a ${pokemonTypesText} type pokemon.`;
-    content.appendChild(text);
-
-    card.appendChild(content);
-
-    container.appendChild(card);
+    generateCards(pokemon);
   });
 };
 
-const handleInputChange = (): void => {
+const displayFilteredCards = (): void => {
   const filteredPokemon = pokemonArray.filter((pokemon) =>
     pokemon.name.toLowerCase().includes(input.value.toLowerCase())
   );
   container.innerHTML = "";
   filteredPokemon.forEach((pokemon) => {
-    const pokemonTypes = pokemon.types;
-    const pokemonTypesText: string =
-      pokemonTypes.length === 1
-        ? `${pokemonTypes[0]}`
-        : `${pokemonTypes[0]} & ${pokemonTypes[1]}`;
-    const pokemonName =
-      pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    const image = document.createElement("img");
-    image.src = pokemon.sprite;
-    image.alt = pokemon.name;
-    card.appendChild(image);
-
-    const content = document.createElement("div");
-    content.classList.add("card__content");
-
-    const heading = document.createElement("h2");
-    heading.classList.add("card__heading");
-    heading.innerText = pokemonName;
-    content.appendChild(heading);
-
-    const text = document.createElement("p");
-    text.classList.add("card__text");
-
-    text.innerText = `${pokemonName} (#${pokemon.id}) is a ${pokemonTypesText} type pokemon.`;
-    content.appendChild(text);
-
-    card.appendChild(content);
-
-    container.appendChild(card);
+    generateCards(pokemon);
   });
 };
 
-document.addEventListener("DOMContentLoaded", generatePokemonCards);
-input?.addEventListener("input", handleInputChange);
+document.addEventListener("DOMContentLoaded", displayAllCards);
+input.addEventListener("input", displayFilteredCards);
